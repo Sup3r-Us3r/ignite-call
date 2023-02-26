e424bbe94c542a8ceeff52fac9e1ceca6eab3abe
Sup3r-Us3r <maydersonmello@gmail.com>
2023-02-26 12:01:17 -0300

    add page extensions in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
   experimental: {
     appDir: true,
   },
