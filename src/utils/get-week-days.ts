b9532a87654760dd95b3ecfa04fb54eaf7020b04
Sup3r-Us3r <maydersonmello@gmail.com>
2023-02-28 17:52:09 -0300

    add new param to return short version of week day

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,11 +1,22 @@
-function getWeekDays() {
+interface IGetWeekDaysParams {
+  short?: boolean;
+}
+
+function getWeekDays({ short = false }: IGetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' });
 
   return [...new Array(7).keys()]
     .map(day => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map(weekDay =>
-      weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1)),
-    );
+    .map(weekDay => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase();
+      } else {
+        return weekDay
+          .substring(0, 1)
+          .toUpperCase()
+          .concat(weekDay.substring(1));
+      }
+    });
 }
 
 export { getWeekDays };
