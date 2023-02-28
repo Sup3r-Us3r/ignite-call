2b55c8397cb819874b6a337c9f7647a68d252c88
Sup3r-Us3r <maydersonmello@gmail.com>
2023-02-28 20:46:22 -0300

    deal with time in past

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -66,9 +66,13 @@ async function handler(request: NextApiRequest, response: NextApiResponse) {
   });
 
   const availableTimes = possibleTimes.filter(time => {
-    return !blockedTimes.some(
+    const isTimeBlocked = blockedTimes.some(
       blockedTime => blockedTime.date.getHours() === time,
     );
+
+    const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date());
+
+    return !isTimeBlocked && !isTimeInPast;
   });
 
   return response.json({ possibleTimes, availableTimes });
