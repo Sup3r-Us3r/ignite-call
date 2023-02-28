042c5bf3e3396be071c2e0e35fe654071d811c93
Sup3r-Us3r <maydersonmello@gmail.com>
2023-02-28 18:04:32 -0300

    install and configure react query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,15 +3,19 @@ import '../lib/dayjs';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
+import { queryClient } from '@/lib/react-query';
 import { globalStyles } from '@/styles/global';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 globalStyles();
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 };
 
