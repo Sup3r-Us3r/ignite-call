562916c1b2530afe6a268c188532d56fd444a197
Sup3r-Us3r <maydersonmello@gmail.com>
2023-02-28 17:54:49 -0300

    create schedule page

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,67 @@
+import type { GetStaticPaths, GetStaticProps } from 'next';
+
+import { prisma } from '@/lib/prisma';
+import { Avatar, Heading, Text } from '@ignite-ui/react';
+
+import { ScheduleForm } from './ScheduleForm';
+import { Container, UserHeader } from './styles';
+
+interface IScheduleProps {
+  user: {
+    name: string;
+    bio: string | null;
+    avatarUrl: string | null;
+  };
+}
+
+const SchedulePage = ({ user }: IScheduleProps) => {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl || 'https://github.com/Sup3r-Us3r.png'} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+
+      <ScheduleForm />
+    </Container>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps<IScheduleProps> = async ({
+  params,
+}) => {
+  const username = String(params?.username);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  };
+};
+
+export default SchedulePage;
